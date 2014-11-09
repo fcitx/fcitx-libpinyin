799cc73b92d4485cad8d1a1cee8eb691099b849c
Henry Hu <henry.hu.sh@gmail.com>
2014-11-09 12:14:38 -0500

    [libpinyin] define _GNU_SOURCE for getline()
    getline() requires _GNU_SOURCE to be defined

--- src/eim.cpp
+++ src/eim.cpp
@@ -18,6 +18,8 @@
  *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
  ***************************************************************************/
 
+#define _GNU_SOURCE
+
 #include <sstream>
 #include <stdlib.h>
 #include <stdio.h>
